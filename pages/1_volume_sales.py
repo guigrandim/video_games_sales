b4999507a5bf83681372f6103128211cbad26555
Guilherme Grandim <gui.grandim@gmail.com>
2026-04-21 21:08:14 -0300

    Clean libray and change pie_chart to a dataframe

--- pages/1_volume_sales.py
+++ pages/1_volume_sales.py
@@ -3,14 +3,11 @@
 #==================================
 
 import pandas as pd
-import os
-import datetime
 import streamlit as st
-import folium
 from PIL import Image
 import plotly.express as px
-import re
 from utils.data_loader import dataset_clean
+from utils.sidebar import render_sidebar
 from datetime import date
 import plotly.graph_objects as go
 
@@ -23,10 +20,6 @@ st.set_page_config(page_title = 'Volume e Vendas', page_icon = '📉', layout = 
 #===================================
 # Functions
 #===================================
-
-@st.cache_data
-def get_data():
-    return dataset_clean()
 
 def market_share_per_region(df1):
     sales_global_per_region = pd.Series({
@@ -50,30 +43,7 @@ def market_share_per_region(df1):
     
     sales_global = sales_global_per_region[['region', 'sales_per_region']]
     
-    return fig
-
-def market_share_per_holding(df1):
-    df_market_share_holdings = df1.loc[:, ['total_sales', 'holdings_publisher']]
-    df_market_share_holdings = df_market_share_holdings[df_market_share_holdings['total_sales'] > 0]
-    
-    sales_per_holdings = (
-        df_market_share_holdings.groupby('holdings_publisher')['total_sales']
-        .sum()
-        .sort_values(ascending = False)
-        .reset_index()
-    )
-    
-    sales_per_holdings['market_share_per_holding'] = ((sales_per_holdings['total_sales'] / sales_per_holdings['total_sales'].sum()) * 100).round(2)
-    sales_per_holdings = sales_per_holdings.iloc[:6]
-    
-    fig = go.Figure(data=[go.Pie(
-        labels = sales_per_holdings['holdings_publisher'],
-        values = sales_per_holdings['market_share_per_holding'],
-        hole=.3
-    )])
-    fig.update_layout(title_text = 'Market Share por Holding (Top 5)')
-    
-    return fig
+    return fig, sales_global
 
 def release_game_per_year(df1):
     release_game_year = df1.loc[:, ['title', 'release_date']]
@@ -182,90 +152,46 @@ def holdings_region_function(df1):
 # Select Directory - Load Files and Clean Dataset
 #===============================================
 
-df1 = get_data()
+df1 = dataset_clean()
 
 #======================================
-# PHASE 2 - Create a Sidebar
+# Create a Sidebar
 #======================================
 
-#1. Load Logo Page
-
-#2. Configuration Filters
-#2.1 - Date
-data_minima = date(2024, 12, 31)
-data_maxima = date(1970, 1, 1)
-
-#2.2 - Genre
-genero = sorted(df1['genre'].unique())
-
-#2.3 - Console
-console = df1.groupby('console')['total_sales'].sum().sort_values(ascending = False).reset_index()
-console = console['console']
-
-#2.4 - Empresa
-manufacture = df1.groupby('manufacture')['total_sales'].sum().sort_values(ascending = False).reset_index()
-manufacture = manufacture['manufacture']
-
-#2.5 - Geração
-generation = sorted(df1['generation'].unique())
-
-#3. Create a Sidebar
-with st.sidebar:
-    st.title('BrasCo - Gaming Ltd.')
-    st.divider()
-
-    data_minima, data_maxima = st.slider("Filtar por data", data_minima, data_maxima, (data_minima, data_maxima))
-    
-    with st.expander("Filtros Avançados"):
-        filter_genero = st.multiselect("Selecione o Genero", options=genero, default=genero)
-        filter_console = st.multiselect("Selecione o Console", options=console, default=console)
-        filter_manufacture = st.multiselect("Selecione a Empresa", options=manufacture, default=manufacture)
-        filter_generation = st.multiselect("Selecione a Geração", options=generation, default=generation)
-
-    st.divider()
-    
-    st.sidebar.markdown('#### Powered by GG Solution \nOwner: Guilherme Grandim')
-
-#4. Filter Functions
-
-df1 = df1[df1['release_date'].between(pd.Timestamp(data_minima), pd.Timestamp(data_maxima))]
-df1 = df1[df1['genre'].isin(filter_genero)]
-df1 = df1[df1['console'].isin(filter_console)]
-df1 = df1[df1['manufacture'].isin(filter_manufacture)]
-df1 = df1[df1['generation'].isin(filter_generation)]
+df1, filter_genero, filter_console, filter_manufacture, filter_generation = render_sidebar()
 
 #=======================================
-# PHASE 3 - Create a Body Page
+# Create a Body Page
 #=======================================
 
-#1. Create a Header
+#Create a Header
 st.title ('Sales & Volume')
 
-#2. Bulletpoins in Page
+#Bulletpoins in Page
 
-#3 Create a Tabs in Page
+#Create a Tabs in Page
 
 tab1, tab2 = st.tabs(["Visão Geral Vendas", "Visão Geral Lançamentos"])
 width_default = 350
 
-#3. Tab 1 - First Line
+#Tab 1 - Sales Tab
 with tab1:
     with st.container():
-        col1, col2 = st.columns(2)
-        fig_sales_market_share = market_share_per_region(df1)
-        fig_holding_market_share = market_share_per_holding(df1)
-
+        col1, col2 = st.columns([3, 2])
+        fig_sales_market_share, df_sales_global = market_share_per_region(df1)
+        
         with col1:
             st.plotly_chart(fig_sales_market_share, use_container_width=True)
-
+        
         with col2:
-            st.plotly_chart(fig_holding_market_share, use_container_width=True)
+            st.markdown("<br><br><br><br><br>", unsafe_allow_html=True)
+            st.dataframe(df_sales_global, use_container_width = True, hide_index = True)
 
-#4. Tab1 - Second Line
     with st.container():
         fig_sales_games_per_year = sales_game_per_year(df1)
         st.plotly_chart(fig_sales_games_per_year, use_container_width=True)
 
+#Tab2 - Release Tab
 with tab2:
     with st.container():
         col5, col6, col7 = st.columns(3)
