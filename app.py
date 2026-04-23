8d1b7865fff33a59a170288fd72a31aff2c49582
Guilherme Grandim <gui.grandim@gmail.com>
2026-04-23 08:20:51 -0300

    Update description in app.py

--- app.py
+++ app.py
@@ -1,3 +1,21 @@
 import streamlit as st
 
+st.set_page_config(page_title="Home", page_icon="🏡", layout = 'wide')
+
 st.title("BrasCo - Gaming Ltd.")
+
+st.markdown(
+    """
+    Video Games Sales Dashboard foi construido para acompanhamento das métricas da evolução do mercado de video games (1970 - 2024).
+    ### Como utilizar essa dashboard?
+    - Pag 1: (COLOCAR NOME)
+        - Explicação de Indicadores
+    - Pag 2:(COLOCAR NOME)
+        - Explicação de Indicadores
+    - Pag 3:(COLOCAR NOME)
+        - Explicação de Indicadores
+    - Pag 4:(COLOCAR NOME)
+
+    ### Ask for Help
+    - Head GG Solution: 📩 @gui.grandim or ℹ️ guilherme-grandim
+    """)
